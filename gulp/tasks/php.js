eed90c65da6e530446e01b945bf6da5da61e92f3
Kucherak Vasyl <vasil.kucherak@gmail.com>
2022-07-12 15:47:30 +0300

    first commit

---
+++ gulp/tasks/php.js
@@ -0,0 +1,5 @@
+//! Копіювання файлів PHP із джерела до папки з результатом
+export const php = () => {
+    return app.gulp.src(app.path.src.php)
+        .pipe(app.gulp.dest(app.path.dist.php))  //? destination (з англ. "шлях призначення")
+}
---
+++ gulp/tasks/reset.js
@@ -0,0 +1,6 @@
+//! Оновлення файлів у папці з результатом
+import del from 'del'; //? модуль для видалення файлів
+
+export const reset = () => {
+    return del(app.path.clean); //? (clean: distFolder)
+}
