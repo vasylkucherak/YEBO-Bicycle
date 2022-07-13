55796ba84eee27bf90e1111f22c8e18bf3b50e52
Kucherak Vasyl <vasil.kucherak@gmail.com>
2022-07-13 11:33:07 +0300

    v.0.6.1(added slider in the page)

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ import touchOrCursor from "./modules/_touchOrCursor.js"; //? Перевірка 
 //? Додаткові скрипти =================================================================================================================================
 import headerMenu from "./modules/_headerMenu.js"; //? Меню шапки
 //import spoiler from "./modules/_spoiler.js"; //? Спойлер (або акордіон)
-//import swiperSlider from "./modules/_swiperSlider.js" //? Слайдер Swiper
+import swiperSlider from "./modules/_swiperSlider.js" //? Слайдер Swiper
 //import slickSlider from "./modules/_slickSlider.js" //? Слайдер Slick
 //import form from "./modules/_form.js"; //? Форма
 //import popup from "./modules/_popup.js"; //? Модальне вікно
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         acordion: false
     }); */
 
-    //swiperSlider();
+    swiperSlider();
 
     //slickSlider();
 
